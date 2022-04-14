1a4f8f6773e25c1013395f2d835021b595d46f48
AliGund <gundogan.ali62@gmail.com>
2022-04-14 14:12:40 +0200

    Ich habe ein Programm geschrieben welches vielleicht funktioniert. Einlesen wurde erstellt. Es übergibt die ersten zwei Zeilen eines Dokumentes.

--- Project/einlesen.cpp
+++ Project/einlesen.cpp
@@ -1,10 +1,33 @@
-//
-// Created by chris on 4/13/2022.
-//
-//#todo: ali
-
-#include <cstdio>
-
-bool einlesen() {
-    printf("eingelesen");
+#include "stdio.h"
+#include "string"
+#include <iostream>
+using namespace std;
+string dawdwa[4];
+void ausgabe(string email)
+{
+    std::cout<< email;
 }
+void einlesen(){
+    int counter;
+    char dateiname2[] = "musterdatei_testbuchung.txt";
+    char inhalt[] = "";
+    FILE *datei2;
+    datei2 = fopen(dateiname2, "r");
+    if(datei2 != NULL){
+        while(feof(datei2) == NULL){
+            fgets(inhalt, 50, datei2);
+            /*printf("%s", inhalt);*/
+            dawdwa[counter] = inhalt;
+            counter++;
+        }
+    }
+    for(int i=0; i<2;i++)
+    {
+        ausgabe(dawdwa[i]);
+    }
+}
+
+int main(void){
+    einlesen();
+}
+
