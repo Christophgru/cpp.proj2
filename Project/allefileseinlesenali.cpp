cfc5aaea8bcdb536730cb18b78e2369456523b4f
Christophgru <christoph@familie-gruender.de>
2022-04-14 11:29:18 +0200

    ali file erstellt

---
+++ Project/allefileseinlesenali.cpp
@@ -0,0 +1,10 @@
+//
+// Created by chris on 4/14/2022.
+//
+#import "testfileabbildung.cpp"
+
+test* import_files_from_link( char* pathueberwacherpfad){
+    //todo: Ali : Lese Files aus, erstelle für jedes file ein test objekt, pack die ins array, gebe array zurück
+    test arrayrückgabe [40];
+    //befüllen
+}
--- Project/einlesen.cpp
+++ Project/einlesen.cpp
@@ -5,7 +5,9 @@
 
 #include <cstdio>
 #include "testfileabbildung.cpp"
+#include "allefileseinlesenali.cpp"
 
 void oneinlesen(char* pathueberwacherpfad,test testarray[40]) {
+    test frompath [40]=import_files_from_link(pathueberwacherpfad)
     printf("eingelesen\n");
 }
